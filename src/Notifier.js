1a87f884fff75dfcdb226f92b8ba234bdcfa0d4c
Gabriel <gabrielzanon77@outlook.com>
2021-06-06 13:49:05 -0400

    implementação da parte 4

--- src/Notifier.js
+++ src/Notifier.js
@@ -1,10 +1,12 @@
 const Notifier = {
   async init() {
     const permission = await Notification.requestPermission()
+    
     if (permission !== 'granted') {
       throw new Error('Permissão Negada')
     }
   },
+
   notify({title, body, icon}) {
      return () => new Notification(title, {
       body,
